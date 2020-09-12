438da91d8ea025846ee841863ccaa2c14bf4e598
kariyahunter <karhunter@csumb.edu>
2020-09-12 13:59:06 -0700

    edited lab 4 page

---
+++ lab4/js/lab.js
@@ -0,0 +1,5 @@
+// Print Onclick button should enact given function when = true
+   // Print function to hovered = false, when not hovering
+   	// Print event for when transition ends
+   	// Attribute variables values
+// Function should follow given CSS classlist
