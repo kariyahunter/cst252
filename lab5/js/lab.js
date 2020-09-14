4e559c129e91f28d81bd3cf48966417605809bda
kariyahunter <karhunter@csumb.edu>
2020-09-14 16:49:16 -0700

    edited lab 4

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/*
+ *  Author: Kariya Hunter <karhunter@csumb.edu>
+ * Created: 14 September
+ * Licence: Public Domain
+ */
+
+ // Define Variables
+ var make = "Honda";
+ var model = "Accord";
+ var color = "White";
+ var year = 1998;
+
+ // calculate
+ var age = 2020 - year;
+
+ // output
+ document.writeln("Make: " + make + "<br>");
+ document.writeln("Model: " + model + "<br>");
+ document.writeln("Color: " + color + "<br>");
+ document.writeln("Year: " + year + "<br>");
+ document.writeln("Age: " + age + " years<br>");
