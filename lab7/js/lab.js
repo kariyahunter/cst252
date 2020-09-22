1c24ae7206a0870cc59de2fba79f3427b2c24ca1
kariyahunter <karhunter@csumb.edu>
2020-09-21 17:14:37 -0700

    lab 7

---
+++ lab7/js/lab.js
@@ -0,0 +1,22 @@
+// sortUserName - a function that takes user input and sorts the letters
+function sortUserName() {
+  var userName = window.prompt("Please write your name!");
+  console.log("userName =", userName);
+
+  //split string to array
+  var nameArray = userName.split('');
+  console.log("nameArray =", nameArray);
+
+  //sort array
+  var nameArraySort = nameArray.sort();
+  console.log("nameArraySort =", nameArraySort);
+
+  //join array back to a string
+  var nameSorted = nameArraySort.join('');
+  console.log("nameSorted =", nameSorted);
+  return nameSorted;
+}
+
+//Output
+document.writeln("Here's your new name!")
+  sortUserName(), "</br>";
