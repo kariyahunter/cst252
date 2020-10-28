2c7d427c3864542964f40df3588d622bd80c8f50
kariyahunter <70294284+kariyahunter@users.noreply.github.com>
2020-10-28 16:55:52 -0700

    lab17

---
+++ lab17/js/lab.js
@@ -0,0 +1,17 @@
+/*
+ *  Author: Kariya Hunter <karhunter@csumb.edu>
+ * Created: 14 September
+ * Licence: Public Domain
+*/
+
+$("#button1").click(function(){
+	$("#button1").toggleClass("pink");
+})
+
+$("#button2").click(function(){
+	$("#button2").toggleClass("lightblue");
+})
+
+$("#button3").click(function(){
+	$("#button3").toggleClass("lightgreen");
+})
