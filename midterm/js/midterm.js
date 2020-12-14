2f87dc6be061a27376ab95083ae0e30f9112ad0c
kariyahunter <70294284+kariyahunter@users.noreply.github.com>
2020-12-13 17:08:19 -0800

    final

--- midterm/js/midterm.js
+++ midterm/js/midterm.js
@@ -25,3 +25,28 @@ $(".hoverable").hover(
     $("p#" + event.currentTarget.id).css("visibility", "hidden")
   }
 )
+
+endpoint="https://api.kanye.rest"
+
+$("#button").click(function(){
+	$.ajax({
+   // The URL for the request
+ url: endpoint,
+ // The data to send (will be converted to a query string)
+ data: { id: 123},
+ // Whether this is a POST or GET request
+ type: "GET",
+ // The type of data we expect back
+ dataType : "json",
+  })
+// If the request succeeds
+.done(function( data ) {
+ //alert("Success!");
+ var quote = data.quote;
+ $("#output").append("<p>" + quote)
+})
+// If the request fails
+.fail(function( xhr, status, errorThrown ) {
+ console.log(errorThrown + " Status:" + status );
+});
+})
